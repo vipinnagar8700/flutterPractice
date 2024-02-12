a2f15b01367a1767a311ad41c395071f975a7062
Vipin nagar <vipinnagar8700@gmail.com>
2024-02-12 16:42:10 +0530

    Header & Footer complete

---
+++ lib/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Life Care App"),
+      ),
+      body: Center(
+          child: Container(
+        child: Text("Welcome to VIP Inovations!"),
+      )),
+      drawer: Drawer(),
+    );
+  }
+}
